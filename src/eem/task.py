43930b53063f8f7b215b5c157a9ab399f811e231
sstroemer <stefan.stroemer@gmail.com>
2024-12-23 00:48:50 +0100

    feat: track epw and basic description in task

--- src/eem/task.py
+++ src/eem/task.py
@@ -1,9 +1,15 @@
+from .entsoepywrapper import EntsoePyWrapper
+from .analysis.describe import describe
+
 class Task:
-    def __init__(self, *, countries, kpis, t0, t1):
+    def __init__(self, epw: EntsoePyWrapper, *, countries, kpis, t0, t1):
         self._countries = countries
         self._kpis = kpis
         self._t0 = t0
         self._t1 = t1
+
+        self._epw = epw
+        self._description = describe(self._epw, self)
 
     @property
     def countries(self):
@@ -20,3 +26,7 @@ class Task:
     @property
     def t1(self):
         return self._t1
+
+    @property
+    def description(self):
+        return self._description
